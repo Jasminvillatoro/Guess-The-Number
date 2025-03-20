d021d470287cfd29383bd2b7e827b1e0939533c7
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2025-03-20 12:29:56 -0500

    Better logic

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
-import random #imports random module
-
+"""Module providing the randint method """
+import random 
 # Welcomes the user and gives information about the game
 def display_title():
-
 	print('Welcome to Guess The Number Game')
 	print('Guess a number from 1 - 10')
 
-# logic of the game. Let the user know if guess is too high, too low, or correct
-def play_game(status):
-	 
-	random_number = random.randint(1, 10)
-	users_guess= 0
-	 
-	while users_guess != random_number:
+# Logic of the game. Lets the user know if guess is too high, too low, or correct
+# In this function we use the randint() method which returns a random number from 1 - 10
+def play_game():
+		random_number = random.randint(1, 10)
+		user_guess = 0
 
-		users_guess = int(input('Enter a number from 1 - 10. '))
+		while user_guess != random_number:
+			user_guess = int(input('Enter a number from 1 - 10. '))
 
-		if users_guess > random_number:
-			print('Too high')
+			if user_guess > random_number:
+				print('Too high')
 
-		elif users_guess < random_number:
-			print('Too low')
+			elif user_guess < random_number:
+				print('Too low')
 
-	print('You Guessed It!')
-	status = 'no'
-	return  status
+		print('You Guessed It!')
+		# Once, returned user will get prompted to play the game again or not.
+		return  main()
 
-# Main function for the user to play or exit game
+# Main function
+# Prompts the user to play the random number game or to exit 
 def main():
-
 	display_title()
-	play = input('Would you like to play?   Enter YES OR NO. ')
-
-	while play.lower() == 'yes':
-		return play_game(play)
+	user_play = input('Would you like to play?   Enter YES OR NO. ')
+	# While loop. if the user enters yes the play game function will be called.
+	# User enters no the last statement will run.
+	while user_play.lower() == 'yes':
+		return play_game()
 
 	return print('Exiting Game.. Have a good day')
 
